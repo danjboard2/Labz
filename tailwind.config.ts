74f4b9ed7bf220b447f3bafa28ad4c4c2eda17c8
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-09-25 22:07:27 +0100

    labz first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,23 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+      xxl: '1920px',
+    },
+    colors: {
+      'primary': '#FF3D00',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'homepage': "url('/media/images/homepage-bg.jpg')",
       },
     },
   },
