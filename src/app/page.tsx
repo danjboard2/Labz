9c18ae573c9fec5110d922a3711e5ed41e7a35e8
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-09-25 23:10:24 +0100

    swipe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex bg-homepage bg-cover min-h-screen flex-col items-center justify-center p-24">
-     <div className="slide-unlock contents relative">
+     <div className="slide-unlock flex relative">
       <div className="pulsing-rectangle relative flex w-[59px] h-[62px]">
         <Image className="absolute block" src="/media/images/labz-rectangle-slide.png" width={59} height={61} alt={'Welcome to Labz'}/>
         <Image className="pulse absolute" src="/media/images/labz-rectangle-slide-outer1.png" width={82} height={84} alt={'Welcome to Labz'}/>
         <Image className="pulse2 absolute" src="/media/images/labz-rectangle-slide-outer2.png" width={107} height={110} alt={'Welcome to Labz'}/>
         <Image className="pulse3 absolute" src="/media/images/labz-rectangle-slide-outer3.png" width={147} height={152} alt={'Welcome to Labz'}/>
       </div>
-      <div className="arrow-right absolute left-[100px]">
+      <div className="arrow-right absolute left-[150px] top-[20px] w-[46px]">
         <Image src="/media/images/arrow-right.png" width={46} height={20} alt={'Swipe right'}/>
       </div>
      </div>
