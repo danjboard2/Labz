0c935c45198e2c995402c2a717360262925fe997
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-09-26 01:47:28 +0100

    counting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ class ParentComponent extends Component<{}, ParentState> {
     <main className="flex bg-homepage bg-cover min-h-screen flex-col items-center justify-center p-24">
      <div className="slide-unlock flex relative">
       <div className="pulsing-rectangle relative flex w-[59px] h-[62px]">
-        <div className="counter absolute left-[12px] top-[10px] text-[10px] font-bold text-[#fff]">{this.state.count}</div>
+        <div className="counter absolute left-[12px] top-[10px] text-[8.9px] font-bold text-[#fff]">{this.state.count}</div>
         <Image className="absolute block" src="/media/images/labz-rectangle-slide.png" width={59} height={61} alt={'Welcome to Labz'}/>
         <Image className="pulse absolute" src="/media/images/labz-rectangle-slide-outer1.png" width={82} height={84} alt={'Welcome to Labz'}/>
         <Image className="pulse2 absolute" src="/media/images/labz-rectangle-slide-outer2.png" width={107} height={110} alt={'Welcome to Labz'}/>
