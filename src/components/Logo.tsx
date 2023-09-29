bc8288947dbf947abb0101ed26341d49a268f303
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-09-29 23:05:53 +0100

    labz logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image'
 
 export default function Logo(){
     return (
         <div className="logo relative select-none">
-        <div className="pulsing-rectangle relative flex w-[61px] h-[62.5px] float-right">
+            <Image src="/media/images/labz-logo.svg" width={94.3} height={58} alt="Labz Logo"/>
+       {/* <div className="pulsing-rectangle relative flex w-[61px] h-[62.5px] float-right">
               <div className=" w-[61px] h-[62.5px]] -z-[1] pointer-events-none"><p className="counter absolute left-[6.4px] top-[5px] text-[6.4px] font-bold text-[#fff] !opacity-100">57</p>
                  <div className="logo-rect absolute block -z-[1] pointer-events-none w-[61px] h-[62.5px] !border-[4.73px]">
                     <div className="right-top !w-[4.73px] !rounded-tr-[2px]"></div>
@@ -10,7 +12,7 @@ export default function Logo(){
                  </div>
               </div>
               </div>
-              <div className="rsbcText !top-[0.5px] !opacity-100 !text-[39.96px] !ml-[9.8px]">Labz</div>
+        <div className="rsbcText !top-[0.5px] !opacity-100 !text-[39.96px] !ml-[9.8px]">Labz</div> */}
         </div>
     )
 }
