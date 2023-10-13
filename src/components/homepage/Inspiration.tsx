309e5aec64ca6d51db6ebe44647a04a6cb149c00
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-10-13 22:32:45 +0100

    finiosh inspirational quotes and begin team

--- src/components/homepage/Inspiration.tsx
+++ src/components/homepage/Inspiration.tsx
@@ -1,8 +1,10 @@
 import InspirationAnims from './section/inspiration'
+import Team from './section/team'
 import { gsap } from "gsap";
 import React, {useLayoutEffect} from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "../../styles/Inspiration.css";
+import Image from 'next/image'
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -13,27 +15,72 @@ export default function Inspiration() {
         // create our context. This function is invoked immediately and all GSAP animations and ScrollTriggers created during the execution of this function get recorded so we can revert() them later (cleanup)
         let ctx = gsap.context(() => {
     
-                gsap.to(".inspiration", {
+                gsap.to(".combined", {
                     scrollTrigger: {
-                trigger: ".inspiration",
-                pin: ".inspiration",
+                trigger: ".combined",
+                pin: ".combined",
                 start: "top top",
-                end: "+=1900",
+                end: "+=6000",
                 pinSpacing: "margin", // Disable automatic spacing adjustment
                 scrub:true,
                 //markers:true,
-                id: "inspiration",
+                id: "combined",
                     }
         });
+        gsap.fromTo(
+            '.inspo-bg',
+            {
+              opacity: 0,
+            },
+            {
+              opacity: 1,
+              scrollTrigger: {
+                trigger: '.inspiration',
+                start: 'top+=300px bottom-=700px',
+                end: 'top+=500px bottom-=900px',
+                scrub: true,
+              // markers: true,
+                id: 'Inspo background opacity',
+              },
+            }
+          );
+      
+          gsap.fromTo(
+            '.inspo-bg',
+            {
+              top: '50%',
+            },
+            {
+              top: '0%',
+              scrollTrigger: {
+                trigger: '.inspiration',
+                start: 'top bottom-=500px',
+                end: 'top+=500px bottom-=200px',
+                scrub: true,
+                //markers: true,
+                id: 'Inspo background position',
+              },
+            }
+          );
     },); // <- IMPORTANT! Scopes selector text
-        
+    
+    
     return () => ctx.revert(); // cleanup
+
+    
     }, []); // <- empty dependency Array so it doesn't re-run on every render
 
+    
     return (
         <>
-        <section className="inspiration bg-black w-full min-h-full block pb-[5000px]">
-            <InspirationAnims/>
+        <section className="combined ">
+            <section className="inspiration bg-black w-full min-h-full block relative overflow-hidden pb-[5000px]">
+                <section className="inspo-bg absolute z-0 left-0 right-0 top-0 min-h-screen w-full bg-[url('/media/images/homepage-bg.jpg')] bg-cover"></section>
+                    <section className="inspiration-inner relative min-h-full h-screen">
+                        <InspirationAnims/>
+                        <Team/>
+                    </section>
+            </section>
         </section>
         </>
     )
--- src/components/homepage/section/team.js
+++ src/components/homepage/section/team.js
@@ -9,7 +9,7 @@ import Image from 'next/image'
 // Register the GSAP plugins
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, MotionPathPlugin);
 
-export default function InspirationAnims() {
+export default function TeamAnims() {
   useLayoutEffect(() => {
     // Create a timeline context
 
@@ -17,23 +17,9 @@ export default function InspirationAnims() {
   // Create a timeline context
   let ctx = gsap.context(() => {
 
-    gsap.to(".quote-wrap h1.quote", {
-      lineHeight: "130px",
-      paddingTop:"0px",
-      rotation: 0,
-      scrollTrigger: {
-        trigger: ".inspiration-inner",
-        start: "top bottom-=100px",
-        end: "top+=800px bottom-=400px",
-        scrub: true,
-        //markers: true,
-        id: "quote"
-      }
-    });
-
-    gsap.to(".quote-wrap h1.quote", {
-      filter: "blur(80px)",
-      opacity:0,
+    gsap.to(".team-inner-left", {
+      filter: "blur(0px)",
+      opacity:1,
       rotation: 0,
       scrollTrigger: {
         trigger: ".inspiration-inner",
@@ -44,7 +30,19 @@ export default function InspirationAnims() {
         id: "quote"
       }
     });
-
+    gsap.to(".team-inner-right", {
+      filter: "blur(0px)",
+      opacity:1,
+      rotation: 0,
+      scrollTrigger: {
+        trigger: ".inspiration-inner",
+        start: "bottom+=450px bottom-=100px",
+        end: "bottom+=1000px bottom-=400px",
+        scrub: true,
+        //markers: true,
+        id: "quote"
+      }
+    });
   });
 
   // Revert the context when the component unmounts
@@ -54,9 +52,16 @@ export default function InspirationAnims() {
 
   return (
           <>
-        <div className="quote-wrap h-full absolute flex justify-center items-center top-0 bottom-0 left-0 right-0">
-          <h1 className="relative quote text-9xl text-white w-4/5 font-bold pt-[400px]"><span className="text-primary">“Great</span> things in a business are never done by one person but instead by a <span className="text-primary">team</span> of people.”</h1>
-        </div>
+        <section className="teams-wrap w-full h-full absolute flex justify-center items-center">
+            <article className="flex flex-row w-3/4">
+            <div className="team-inner-left flex w-1/2">
+          <h2 className="relative quote text-9xl text-white w-4/5 font-bold">We are <span className="text-primary">Labz</span></h2>
+          </div>
+          <div className="team-inner-right flex w-1/2">
+          <h2>Team information here</h2>
+          </div>
+          </article>
+          </section>
       </>
   );
 };
