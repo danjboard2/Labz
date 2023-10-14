e5013378e45e6455a79ef72d4bb47f7da0718c6a
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-10-14 10:27:31 +0100

    Begin teams section build

--- src/components/homepage/section/team.js
+++ src/components/homepage/section/team.js
@@ -53,12 +53,38 @@ export default function TeamAnims() {
   return (
           <>
         <section className="teams-wrap w-full h-full absolute flex justify-center items-center">
-            <article className="flex flex-row w-3/4">
-            <div className="team-inner-left flex w-1/2">
-          <h2 className="relative quote text-9xl text-white w-4/5 font-bold">We are <span className="text-primary">Labz</span></h2>
+            <article className="flex flex-row w-full mx-[93px]">
+            <div className="team-inner-left flex w-2/3 flex-col pr-10">
+          <h2 className="relative quote text-9xl text-white font-bold">We are <span className="text-primary">Labz</span></h2>
+          <hr className=" w-5/12 border-[#828282] mt-12 mb-24"/>
+          <div className="member-wrap flex flex-row items-center">
+            <div className="profile-photo w-2/5 max-w-[350px] flex justify-center items-center border-4 border-primary rounded-lg mr-10  bg-[linear-gradient(180deg,_#FF3D00_0%,_rgba(255,61,0,0.00)_100%)]">
+              <Image
+                src="/media/images/team/team-member-1.png"
+                alt="Picture of the team member"
+                width={300}
+                height={300}
+                className="-mt-20 pb-8"
+              />
+            </div>
+            <div className="member-info w-3/5">
+              <div className="member-names flex flex-row pb-8 items-center">
+                <div className="names mr-10">
+                  <h3 className="text-3xl font-bold text-white">Name Surname</h3>
+                  <h4 className="text-3xl font-bold text-primary">CEO/Founder</h4>
+                </div>
+                <div className="linkedin">
+                  <a href="https://www.linkedin.com/in/"><Image src="/media/images/linkedin.svg" width={60} height={60} alt="LinkedIn"/></a>
+                </div>
+              </div>
+              <div className="member-desc text-xl text-white">
+                <p>A distinguished entrepreneur with over a decade of expertise in blockchain technology and a relentless drive to advance the adoption of digital assets. Specialising in innovative ideas and creative solutions propelling the industry forward with a visionary approach, leveraging a global network of esteemed web3 and deep tech professionals.</p>
+                </div>
           </div>
-          <div className="team-inner-right flex w-1/2">
-          <h2>Team information here</h2>
+          </div>
+          </div>  
+          <div className="team-inner-right flex w-1/3">
+            <h2>Other team members here</h2>
           </div>
           </article>
           </section>
