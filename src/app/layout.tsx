0333ca18d1e4e4495a89bb5137c1300f8d86e0bb
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-10-30 13:24:27 +0000

    stop google translate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import '../app/nav-overrides.css';
 
 export const metadata: Metadata = {
   title: 'The Labz',
-  description: 'Web3 Technology Development & Solutions - Providing adoption at scale through cutting-edge decentralized ecosystem and product builds.',
+  description: 'Web3 Technology Development & Solutions - Providing adoption at scale through cutting-edge decentralized ecosystem and product builds.'
 }
 
 export default function RootLayout({
@@ -14,7 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" translate="no" className="notranslate">
+     <meta name="google" content="notranslate" />
       <body className=" bg-[#000]">{children}</body>
     </html>
   )
