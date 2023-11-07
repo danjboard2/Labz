d1d850dc7da2276fbcf39dcc62f6895a911cde30
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-11-07 23:42:41 +0000

    loading page - unused for now

---
+++ src/app/loading/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React, { useEffect } from 'react';
+import './loading.css';
+
+export default function Loading() {
+  useEffect(() => {
+    const boxes = document.querySelectorAll(".box");
+    
+    console.log(boxes);
+
+    const interval = 100;
+    
+    function loading() {
+      boxes.forEach((item, index) => {
+        setTimeout(() => {
+          const htmlItem = item as HTMLElement;
+          htmlItem.style.animation = "loading_anim 1s ease-in-out forwards";
+          // Remove the animation after it's done
+          setTimeout(() => {
+            htmlItem.style.animation = "";
+          }, 1000); // This 1000 should match the duration of the animation
+        }, index * interval);
+      });
+    }
+    
+    const intervalId = setInterval(loading, 1750);
+    
+    // Start the loading animation immediately
+    loading();
+
+    // Cleanup interval on component unmount
+    return () => {
+      clearInterval(intervalId);
+    };
+
+    // Empty dependency array means this effect will only run once (like componentDidMount)
+  }, []);
+
+  return (
+    <>
+      <div className="box"><span className="txt">L</span></div>
+      <div className="box"><span className="txt">o</span></div>
+      <div className="box"><span className="txt">a</span></div>
+      <div className="box"><span className="txt">d</span></div>
+      <div className="box"><span className="txt">i</span></div>
+      <div className="box"><span className="txt">n</span></div>
+      <div className="box"><span className="txt">g</span></div>
+    </>
+  );
+}
