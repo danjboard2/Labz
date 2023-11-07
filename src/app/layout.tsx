2cd15dbf08c76a37f819e1f0089e50a1d07a0056
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-11-07 23:53:26 +0000

    revert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 
-"use client"
 import './globals.css'
+import type { Metadata } from 'next'
 import { Suspense } from 'react';
 import Loading from './loading';
 
+export const metadata: Metadata = {
+  title: 'The Labz',
+  description: 'Web3 Technology Development & Solutions - Providing adoption at scale through cutting-edge decentralized ecosystem and product builds.'
+}
 
 export default function RootLayout({
   children,
